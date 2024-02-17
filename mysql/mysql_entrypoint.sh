9b3701f0b82b6a3bddbdb969ef87617b4bf27ded
makram89 <marasss.kamin@gmail.com>
2024-02-17 14:39:12 +0100

    Clean up -1

--- mysql/mysql_entrypoint.sh
+++ mysql/mysql_entrypoint.sh
@@ -9,30 +9,30 @@ INITIALIZED_FLAG="/var/lib/mysql/initialized.flag"
 
 # Wait for MySQL to be ready
 until mysql -u root --password="${MYSQL_ROOT_PASSWORD}" -e "SELECT 1"; do
-   echo "Waiting for MySQL to be up..."
-   sleep 1
+   echo "Await mysql"
+   sleep 2
 done
 
-# Check for flag
+# Check for flag and if not set configure DB for WP
 if [ ! -f "$INITIALIZED_FLAG" ]; then
 
-    # Configures MySQL
+    # Set up root password
     mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '${MYSQL_ROOT_PASSWORD}';"
 
     # Create a MySQL database and user for WordPress
     mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
     mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
     mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
-    mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+    mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"  
 
-    sed -i 's/localhost/0.0.0.0/g' /etc/mysql/debian.cnf 
-    sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mysql.conf.d/mysqld.cnf
-     
-
+    # Flag after init
     touch "$INITIALIZED_FLAG"
+
+    
 fi
 
-wait $MYSQL_PID
+# Kill base here to run it as mysql in CMD 
+mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
 
-
-# exec $@
+# End script
+exec "$@"
